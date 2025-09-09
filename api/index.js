53770f8b8c2e7bcd25327163789694be404d1885
Derrick Obeng <derickobeng023@gmail.com>
2025-09-09 15:18:27 +0000

    controllers initaited

--- api/index.js
+++ api/index.js
@@ -1,9 +1,60 @@
 import express, { Router } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import roomRouter from "./routes/room.route.js";
+import contactRouter from "./routes/contact.route.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const _dirname = path.resolve();
+
+const app = express();
+const port = 3000;
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Your React app's URL
+    credentials: true, // This is crucial for cookies
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/room", roomRouter);
+app.use("/api/contact", contactRouter);
+app.use(express.static(path.join(_dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
+});
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
